60e7a1c8290ebe2726064c4bcc2738f7edebba4a
princekehinde <princekehinde2345@gmail.com>
2023-05-14 07:03:21 -0700

    exercise2 as started

---
+++ exercise1.js
@@ -0,0 +1,63 @@
+// comments can make code readable
+// Welcome to 30DaysOfJavaScript
+/**
+ *  comments can make code readable, easy to reuse and informative
+ */
+
+// // variable.js
+// declare variables and assign string, boolean, undefined and null data type
+let firstName = 'Asabeneh' // string
+// boolean
+ let isMarried = true; 
+ // undefined
+ let country = undefined;
+ // null
+ let city = null;
+ console.log(firstName, isMarried, country, city)
+
+
+//  datatypes.js 
+// Use the JavaScript typeof operator to check different data types. Check the data type of each variable
+let Name = 'Asabeneh'
+
+let iMarried = true
+
+let Country = undefined
+
+let City = null
+
+console.log(typeof Name, iMarried, Country, City)
+
+// Declare four variables without assigning values
+let variable1;
+let variable2;
+var variable3;
+// const variable4;
+
+
+// Declare four variables with assigned values
+let varwiable1 = 10;
+let varriable2 = "hello";
+const variable3 = true;
+const variiable4 = [1, 2, 3];
+
+// Declare variables to store your first name, last name, marital status, country and age in multiple lines
+
+let firswtName = 'Asabeneh';
+let lastswName = 'Asabeneh';
+let mariwtalStatus = 'Single';
+let counrtry = 'India';
+let agwe = 20;
+
+console.log(firswtName, lastswName, mariwtalStatus, counrtry, agwe);
+
+
+// Declare variables to store your first name, last name, marital status, country and age in a single line
+
+let firstwName = 'Asabeneh';
+let lastName = 'Asabeneh';
+let maritalStatus = 'Single';
+let couwntry = 'India';
+let age = 20;
+
+console.log(firstwName +'' + lastName +'' + maritalStatus +'' + couwntry +'' + age);
