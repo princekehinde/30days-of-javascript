5d6082c13041253ea62de69341d01ea3128a66b6
princekehinde <princekehinde2345@gmail.com>
2023-05-17 08:01:18 -0700

    exercise3 not done

---
+++ exercise3.js
@@ -0,0 +1,31 @@
+//1. Declare firstName, lastName, country, city, age, isMarried, year
+//  variable and assign value to it and use the typeof operator to check different data types.
+
+let firstName = "John";
+let lastName = "Doe";
+let country = "USA";
+let city = "San Francisco";
+let age = 30;
+let isMarried = true;
+let year = 2020;
+
+console.log(typeof firstName);
+console.log(typeof lastName);
+console.log(typeof country);
+console.log(typeof city);
+console.log(typeof age);
+console.log(typeof isMarried);
+console.log(typeof year);
+
+// output
+// string
+// string 
+// string 
+// string 
+// number 
+// boolean
+// number 
+
+//2. check if type of '10' is equal to 10
+
+console.log(10 === 10);
