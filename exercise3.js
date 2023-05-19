559fea031c9b074c2c1c8d467ae795336366f571
princekehinde <princekehinde2345@gmail.com>
2023-05-19 09:53:33 -0700

    done

--- exercise3.js
+++ exercise3.js
@@ -136,22 +136,92 @@
 // console.log(currentDay); // 18
 
 // what is hours now 
+// const hour = new Date();
+// const currentHour = hour.getHours();
+// console.log(currentHour); // 11
 
-const hour = new Date();
-const currentHour = hour.getHours();
-console.log(currentHour); // 11
+// // what is minutes now
+// const minute = new Date();
+// const currentMinute = minute.getMinutes();
+// console.log(currentMinute); // 17
 
-// what is minutes now
+// // find out the number of seconds elapsed from january 1, 1970 to now
+// const jan1 = new Date(1970, 0, 1);
+// const jan2 = new Date();
+// const seconds = jan2.getTime() - jan1.getTime();
+// console.log(seconds); // 1684380221761
 
-const minute = new Date();
-const currentMinute = minute.getMinutes();
-console.log(currentMinute); // 17
 
-// find out the number of seconds elapsed from january 1, 1970 to now
-const jan1 = new Date(1970, 0, 1);
-const jan2 = new Date();
-const seconds = jan2.getTime() - jan1.getTime();
-console.log(seconds); // 1684380221761
+// Write a script that prompt the user to enter base and height of the triangle and calculate an area of a triangle (area = 0.5 x b x h).
+// Enter base: 20
+// Enter height: 10
+// The area of the triangle is 100
+// let base = prompt('Enter base: ');
+// let height = prompt('Enter height: ');
+// let area = (base * height) / 2;
+
+// console.log(area); // 100
+
+// Write a script that prompt the user to enter side a, side b, and side c of the triangle and and calculate the perimeter of triangle (perimeter = a + b + c)
+
+// Enter side a: 5
+// Enter side b: 4
+// Enter side c: 3
+// The perimeter of the triangle is 12
+// let sideA = prompt('enter side a: ')
+// let sideB = prompt('enter side b: ')
+// let sideC = prompt('enter side c: ')
+
+// let perimeter = sideA + sideB + sideC;
+
+// console.log(perimeter); // 12
+
+// Get length and width using prompt and calculate an area of rectangle (area = length x width and the perimeter of rectangle (perimeter = 2 x (length + width))
+// let length = prompt('Enter length: ');
+// let width = prompt('Enter width: ');
+// let area = length * width;
+// let perimeter = 2 * (length + width);
+
+// console.log(area);
+// console.log(perimeter);
+
+// Get radius using prompt and calculate the area of a circle (area = pi x r x r) and circumference of a circle(c = 2 x pi x r) where pi = 3.14.
+// let radius = prompt('Enter radius: ');
+// let area = Math.PI * radius * radius;
+// let circumference = 2 * Math.PI * radius;
+// console.log(area);
+// console.log(circumference);
+
+
+// Calculate the slope, x-intercept and y-intercept of y = 2x -2
+// using prompt.
+// let slope = prompt('Enter slope: ');
+// let intercept = prompt('Enter intercept: ');
+// let y1 = 2;
+// let x1 = 2;
+// let y2 = 6;
+// let x2 = 6;
+// let m = (y2 - y1) / (x2 - x1);
+// console.log(m);
+
+// Slope is m = (y2-y1)/(x2-x1). Find the slope between point (2, 2) and point(6,10)
+let e1 = prompt('Enter x1: ');
+
+let s1 = prompt('Enter y1: ');
+
+let d2 = prompt('Enter x2: ');
+
+let c2 = prompt('Enter y2: ');
+
+let sdlope = (y2 - y1) / (x2 - x1);
+
+console.log(sdlope);
+
+
+
+
+
+
 
 
 
